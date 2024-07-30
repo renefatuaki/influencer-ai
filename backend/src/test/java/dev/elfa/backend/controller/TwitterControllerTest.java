982056f604f5001ad421e315996b2d4053c7258b
René El Fatuaki <rene@elfatuaki.com>
2024-07-30 16:19:54 +0200

    changed use from string to MediaType

--- backend/src/test/java/dev/elfa/backend/controller/TwitterControllerTest.java
+++ backend/src/test/java/dev/elfa/backend/controller/TwitterControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -87,7 +88,7 @@ class TwitterControllerTest {
         );
 
         mvc.perform(MockMvcRequestBuilders.post("/api/twitter")
-                        .contentType("application/json")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content("""
                                 {
                                     "state": "mockState",
