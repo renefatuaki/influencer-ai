73f0792875bfb02fea849dbcb4e3a65204fc432a
René El Fatuaki <rene@elfatuaki.com>
2024-08-06 19:17:49 +0200

    added loading state

---
+++ frontend/src/components/loading.tsx
@@ -0,0 +1,18 @@
+import classes from './loading.module.css';
+
+export default function Loading() {
+  return (
+    <>
+      <div className={`${classes.container} flex flex-col justify-self-center`}>
+        <div className={classes.dot}></div>
+        <div className={classes.dot}></div>
+        <div className={classes.dot}></div>
+        <div className={classes.dot}></div>
+        <div className={classes.dot}></div>
+        <div className={classes.dot}></div>
+        <div className={classes.dot}></div>
+        <div className={classes.dot}></div>
+      </div>
+    </>
+  );
+}
