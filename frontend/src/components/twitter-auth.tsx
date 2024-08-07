d93b41d8ec3dac1b801ceddfd923cf65c85080b9
René El Fatuaki <rene@elfatuaki.com>
2024-08-07 12:45:59 +0200

    optimized workflow by redirecting user to created account and added error messages

--- frontend/src/components/twitter-auth.tsx
+++ frontend/src/components/twitter-auth.tsx
@@ -1,24 +1,31 @@
 'use client';
 
 import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Button, buttonVariants } from '@/components/ui/button';
 import { POST } from '@/lib/fetch';
-import { useRouter } from 'next/navigation';
+import AlertResponse from '@/components/alert-response';
 
 export default function TwitterAuth({ url }: { url: string }) {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
   const state = searchParams.get('state');
   const [open, setOpen] = useState(!!code);
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState(false);
   const router = useRouter();
 
   const clickHandler = async () => {
     const response = await POST('/twitter', { code, state });
-    setOpen(false);
-    router.push(window.location.href.split('?')[0]);
+
+    if (!response.data) {
+      setMessage(response.message);
+      setError(true);
+    } else {
+      router.push(`/management/${response.data.id}`);
+    }
   };
 
   return (
@@ -46,10 +53,19 @@ export default function TwitterAuth({ url }: { url: string }) {
               This action will permanently add your Twitter account to our servers and cannot be undone.
             </DialogDescription>
           </DialogHeader>
-          <DialogFooter>
-            <Button variant="outline" onClick={clickHandler}>
-              Connect
-            </Button>
+          <DialogFooter className="flex flex-wrap gap-4">
+            {error ? (
+              <>
+                <AlertResponse error={true} message={message} />
+                <Link href={url} className={buttonVariants({ variant: 'outline' })}>
+                  Restart Authorization
+                </Link>
+              </>
+            ) : (
+              <Button variant="outline" onClick={clickHandler}>
+                Connect
+              </Button>
+            )}
           </DialogFooter>
         </DialogContent>
       )}
