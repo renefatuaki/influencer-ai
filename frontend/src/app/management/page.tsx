143b30f7d74c4187dcd86cecc4f6019e040d62c5
René El Fatuaki <rene@elfatuaki.com>
2024-08-10 16:46:19 +0200

    moved container to main tag

--- frontend/src/app/management/page.tsx
+++ frontend/src/app/management/page.tsx
@@ -1,14 +1,14 @@
 import { getAuthorizationUrl } from '@/lib/auth';
 import TwitterAuth from '@/components/twitter-auth';
 import { TwitterTable } from '@/components/table/twitter-table';
-import { columns, TableData } from '@/components/table/columns';
+import { columns } from '@/components/table/columns';
 
 export default function Management() {
   const authorizationUrl = getAuthorizationUrl();
 
   return (
     <>
-      <div className="container flex flex-col mx-auto py-10 gap-4">
+      <div className="flex flex-col mx-auto py-10 gap-4">
         <TwitterAuth url={authorizationUrl}></TwitterAuth>
         <TwitterTable columns={columns} />
       </div>
