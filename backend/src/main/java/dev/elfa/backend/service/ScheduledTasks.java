aba3ca0e71c974dd7fc3c09a0b031364f3d1c662
René El Fatuaki <rene@elfatuaki.com>
2024-08-19 19:35:43 +0200

    publish scheduled post generated by ai

--- backend/src/main/java/dev/elfa/backend/service/ScheduledTasks.java
+++ backend/src/main/java/dev/elfa/backend/service/ScheduledTasks.java
@@ -1,27 +1,16 @@
 package dev.elfa.backend.service;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class ScheduledTasks {
     private final TwitterService twitterService;
 
-    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
-
-    private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 900000) // 15 minutes
     public void reportCurrentTime() {
-        String message = String.format("The time is now %s", dateFormat.format(new Date()));
-        log.info(message);
-//        TODO: twitterService.postScheduledTweet();
+        twitterService.postScheduledTweet();
     }
 }
