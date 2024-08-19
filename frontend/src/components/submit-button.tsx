383c505bf065ce014a697244e3459cc080d835d9
René El Fatuaki <rene@elfatuaki.com>
2024-08-19 22:31:30 +0200

    feat: improve SubmitButton component
    - Refactor SubmitButton component to optimize rendering and improve error handling
    - Make `isLoading` prop optional
    - Update code formatting and remove unused imports

--- frontend/src/components/submit-button.tsx
+++ frontend/src/components/submit-button.tsx
@@ -1,29 +1,27 @@
 'use client';
 
-import { useFormStatus } from 'react-dom';
-import { Button } from '@/components/ui/button';
-import { ReactNode } from 'react';
+import {useFormStatus} from 'react-dom';
+import {Button} from '@/components/ui/button';
+import {ReactNode} from 'react';
 import AlertResponse from '@/components/alert-response';
 import Loading from '@/components/loading';
 
 type SubmitButtonProps = {
-  children: ReactNode;
-  state: { error: boolean; message: string };
-  isLoading?: boolean;
+    children: ReactNode;
+    state: { error: boolean; message: string };
+    isLoading?: boolean;
 };
 
-export function SubmitButton({ children, state, isLoading }: SubmitButtonProps) {
-  const { pending } = useFormStatus();
+export function SubmitButton({children, state, isLoading}: Readonly<SubmitButtonProps>) {
+    const {pending} = useFormStatus();
 
-  return (
-    <>
-      <div className="grid w-full gap-4">
-        <Button type="submit" disabled={pending}>
-          {children}
-        </Button>
-        {isLoading && pending && <Loading />}
-        {!pending && state && <AlertResponse error={state.error} message={state.message} />}
-      </div>
-    </>
-  );
+    return (
+        <div className="grid w-full gap-4">
+            <Button type="submit" disabled={pending}>
+                {children}
+            </Button>
+            {isLoading && pending && <Loading/>}
+            {!pending && state && <AlertResponse error={state.error} message={state.message}/>}
+        </div>
+    );
 }
