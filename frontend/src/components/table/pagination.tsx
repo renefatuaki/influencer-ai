4f47a5382e8b9bb1c0ddca8308e9fcdb12d0fffc
René El Fatuaki <rene@elfatuaki.com>
2024-08-19 22:32:23 +0200

    refactor: make `id` prop readonly in Pagination component

--- frontend/src/components/table/pagination.tsx
+++ frontend/src/components/table/pagination.tsx
@@ -8,7 +8,7 @@ interface DataTablePaginationProps<TData> {
   table: Table<TData>;
 }
 
-export function Pagination<TData>({ table }: DataTablePaginationProps<TData>) {
+export function Pagination<TData>({ table }: Readonly<DataTablePaginationProps<TData>>) {
   return (
     <div className="flex items-center justify-end">
       <div className="flex items-center space-x-6 lg:space-x-8">
@@ -36,12 +36,7 @@ export function Pagination<TData>({ table }: DataTablePaginationProps<TData>) {
           Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
         </div>
         <div className="flex items-center space-x-2">
-          <Button
-            variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
-            onClick={() => table.setPageIndex(0)}
-            disabled={!table.getCanPreviousPage()}
-          >
+          <Button variant="outline" className="hidden h-8 w-8 p-0 lg:flex" onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
             <span className="sr-only">Go to first page</span>
             <DoubleArrowLeftIcon className="h-4 w-4" />
           </Button>
