2b3c301c36d7b6e31bcad556b60e3a846af66a37
René El Fatuaki <rene@elfatuaki.com>
2024-08-19 22:31:10 +0200

    refactor: optimize loading component rendering

--- frontend/src/components/loading.tsx
+++ frontend/src/components/loading.tsx
@@ -2,17 +2,15 @@ import classes from './loading.module.css';
 
 export default function Loading() {
   return (
-    <>
-      <div className={`${classes.container} flex flex-col justify-self-center`}>
-        <div className={classes.dot}></div>
-        <div className={classes.dot}></div>
-        <div className={classes.dot}></div>
-        <div className={classes.dot}></div>
-        <div className={classes.dot}></div>
-        <div className={classes.dot}></div>
-        <div className={classes.dot}></div>
-        <div className={classes.dot}></div>
-      </div>
-    </>
+    <div className={`${classes.container} flex flex-col justify-self-center`}>
+      <div className={classes.dot}></div>
+      <div className={classes.dot}></div>
+      <div className={classes.dot}></div>
+      <div className={classes.dot}></div>
+      <div className={classes.dot}></div>
+      <div className={classes.dot}></div>
+      <div className={classes.dot}></div>
+      <div className={classes.dot}></div>
+    </div>
   );
 }
