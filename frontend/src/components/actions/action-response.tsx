0dcf7d7a4abd6e9f0368c0474b46c1ddb345dec1
René El Fatuaki <rene@elfatuaki.com>
2024-08-19 22:31:39 +0200

    refactor: optimize ActionResponse component
    - Update ActionResponse component to optimize rendering and improve code formatting
    - Make `twitterId` prop readonly
    - Remove unused imports and optimize form action handling

--- frontend/src/components/actions/action-response.tsx
+++ frontend/src/components/actions/action-response.tsx
@@ -1,25 +1,23 @@
-import { SubmitButton } from '@/components/submit-button';
-import { useFormState } from 'react-dom';
+import {SubmitButton} from '@/components/submit-button';
+import {useFormState} from 'react-dom';
 
 type ActionResponseProps = {
-  twitterId: string;
-  action: (prevState: any, formData: FormData) => { error: boolean; message: string } | Promise<{ error: boolean; message: string }>;
+    twitterId: string;
+    action: (prevState: any, formData: FormData) => { error: boolean; message: string } | Promise<{ error: boolean; message: string }>;
 };
 
-export default function ActionResponse({ twitterId, action }: ActionResponseProps) {
-  const [state, formAction] = useFormState(action, {
-    error: false,
-    message: '',
-  });
+export default function ActionResponse({twitterId, action}: Readonly<ActionResponseProps>) {
+    const [state, formAction] = useFormState(action, {
+        error: false,
+        message: '',
+    });
 
-  return (
-    <>
-      <form action={formAction} className="grid w-full">
-        <input type="hidden" name="id" value={twitterId} readOnly={true} />
-        <SubmitButton state={state} isLoading={true}>
-          Start Image Generation
-        </SubmitButton>
-      </form>
-    </>
-  );
+    return (
+        <form action={formAction} className="grid w-full">
+            <input type="hidden" name="id" value={twitterId} readOnly={true}/>
+            <SubmitButton state={state} isLoading={true}>
+                Start Image Generation
+            </SubmitButton>
+        </form>
+    );
 }
