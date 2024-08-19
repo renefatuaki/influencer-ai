60fce1da1ae6c1f9b876fcecbb38580d8950f756
René El Fatuaki <rene@elfatuaki.com>
2024-08-19 23:23:08 +0200

    refactor: optimize rendering of Editor component

--- frontend/src/components/editor/editor.tsx
+++ frontend/src/components/editor/editor.tsx
@@ -1,35 +1,37 @@
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from '@/components/ui/card';
-import {SubmitButton} from '@/components/submit-button';
-import {useFormState} from 'react-dom';
-import {ReactNode} from 'react';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { SubmitButton } from '@/components/submit-button';
+import { useFormState } from 'react-dom';
+import { ReactNode } from 'react';
 
 type EditorType = {
-    influencerId: string;
-    title: string;
-    description: string;
-    children?: ReactNode;
-    action: (prevState: any, formData: FormData) => Promise<{ error: boolean; message: string }>;
+  influencerId: string;
+  title: string;
+  description: string;
+  children?: ReactNode;
+  action: (prevState: any, formData: FormData) => Promise<{ error: boolean; message: string }>;
 };
 
-export default function Editor({influencerId, title, description, children, action}: Readonly<EditorType>) {
-    const [state, formAction] = useFormState(action, {
-        error: false,
-        message: '',
-    });
+export default function Editor({ influencerId, title, description, children, action }: Readonly<EditorType>) {
+  const [state, formAction] = useFormState(action, {
+    error: false,
+    message: '',
+  });
 
-    return (
-        <form action={formAction}>
-            <input type="hidden" name="id" value={influencerId} readOnly={true}/>
-            <Card>
-                <CardHeader>
-                    <CardTitle>{title}</CardTitle>
-                    <CardDescription>{description}</CardDescription>
-                </CardHeader>
-                <CardContent>{children}</CardContent>
-                <CardFooter>
-                    <SubmitButton state={state}>Update</SubmitButton>
-                </CardFooter>
-            </Card>
-        </form>
-    );
+  return (
+    <form action={formAction}>
+      <input type="hidden" name="id" value={influencerId} readOnly={true} />
+      <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-2">
+          <CardDescription>{description}</CardDescription>
+          {children}
+        </CardContent>
+        <CardFooter>
+          <SubmitButton state={state}>Update</SubmitButton>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }
