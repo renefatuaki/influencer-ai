3df3055736783aa1a3565d853490a0181369b09b
René El Fatuaki <rene@elfatuaki.com>
2024-08-19 22:30:52 +0200

    refactor: improve error handling in AlertResponse component

--- frontend/src/components/alert-response.tsx
+++ frontend/src/components/alert-response.tsx
@@ -1,32 +1,32 @@
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { AlertCircle, CircleCheckBig } from 'lucide-react';
+import {Alert, AlertDescription, AlertTitle} from '@/components/ui/alert';
+import {AlertCircle, CircleCheckBig} from 'lucide-react';
 
 export type Response = {
-  message: string;
-  error: boolean;
-  link?: string;
+    message: string;
+    error: boolean;
+    link?: string;
 };
 
-export default function AlertResponse({ error, message, link }: Response) {
-  if (message === '') return null;
+export default function AlertResponse({error, message, link}: Readonly<Response>) {
+    if (message === '') return null;
 
-  return (
-    <>
-      {error ? (
-        <Alert variant="destructive">
-          <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>{message}</AlertDescription>
-        </Alert>
-      ) : (
-        <Alert variant="default">
-          <CircleCheckBig className="h-4 w-4 stroke-green-700" />
-          <AlertTitle className="text-green-700">Success</AlertTitle>
-          <AlertDescription className="text-green-700">
-            <a href={link}>{message}</a>
-          </AlertDescription>
-        </Alert>
-      )}
-    </>
-  );
+    return (
+        <>
+            {error ? (
+                <Alert variant="destructive">
+                    <AlertCircle className="h-4 w-4"/>
+                    <AlertTitle>Error</AlertTitle>
+                    <AlertDescription>{message}</AlertDescription>
+                </Alert>
+            ) : (
+                <Alert variant="default">
+                    <CircleCheckBig className="h-4 w-4 stroke-green-700"/>
+                    <AlertTitle className="text-green-700">Success</AlertTitle>
+                    <AlertDescription className="text-green-700">
+                        <a href={link}>{message}</a>
+                    </AlertDescription>
+                </Alert>
+            )}
+        </>
+    );
 }
