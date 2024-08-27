c3a063f355916cdedf38e0b3b4fd0b62257e65e0
René El Fatuaki <rene@elfatuaki.com>
2024-08-28 01:34:24 +0200

    feat: Update remotePatterns in next.config.mjs for dynamic API hostname
    The remotePatterns in next.config.mjs have been updated to allow for a dynamic API hostname. This change enables the use of any hostname for the remote images, rather than being restricted to a specific hostname.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: process.env.API_HOSTNAME,
-                port: process.env.API_PORT,
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '*',
+        port: process.env.API_PORT,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
